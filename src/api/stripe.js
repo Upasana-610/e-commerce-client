598047b685715851dd9ce79271a6156d20daf811
Roshni <sparksroshni610@gmail.com>
2022-08-17 12:03:44 +0530

    Wbhook added

--- src/api/stripe.js
+++ src/api/stripe.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import { showAlert } from "./alerts";
 import { BASE_URL } from "./password";
 
-export const CLIENT_URL = "https://roarupa.netlify.app";
+export const CLIENT_URL = "https://roarupa.netlify.app/";
 
 // const stripe = Stripe(
 //   "pk_test_51LVChDSCF8VTOqb3LoCUt621iQIb3raYCPj4BCiNA5UQ9JXihttODGDLHHE0NE31TLKKG2ts5lXU9rNVpeuzx4cj00dsfpP6HX"
