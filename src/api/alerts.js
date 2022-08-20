247b5372e21120d5f8d2567178f3fe55233f2ab8
Roshni <sparksroshni610@gmail.com>
2022-08-20 23:01:06 +0530

    Email Alerts2

--- src/api/alerts.js
+++ src/api/alerts.js
@@ -5,7 +5,7 @@ export const hideAlert = () => {
 };
 
 // type is 'success' or 'error'
-export const showAlert = (type, msg, time = 5) => {
+export const showAlert = (type, msg, time = 7) => {
   hideAlert();
   const markup = `<div class="alert alert--${type}">${msg}</div>`;
   document.querySelector("body").insertAdjacentHTML("afterbegin", markup);
