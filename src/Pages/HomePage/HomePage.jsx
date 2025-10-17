e28eb4c8a3977a7d08c8248bbe789509f2272cf3
Upasana Pan <190397830+upan_col@users.noreply.github.com>
2025-10-17 21:36:38 +0530

    spinner

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -7,10 +7,20 @@ import Headings from "../../components/HomePage/Heading/Headings";
 import Categories from "../../components/HomePage/Categories/Categories";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router";
+import { ClockLoader } from "react-spinners";
+
 
 function HomePage() {
   let { email } = useSelector((state) =>
     state.user.user !== null ? state.user.user : { undefined }
+  );
+ const override = {
+    display: "block",
+    margin: "20% auto",
+    borderColor: "blue",
+  };
+  let { isLoading } = useSelector((state) =>
+    state.user.isLoading === true ? state.user.isLoading : state.user.isLoading
   );
   let navigate = useNavigate();
 
@@ -25,10 +35,25 @@ function HomePage() {
       {email === "admin@example.com" ? goToAdmin() : ""}
       {console.log(email)}
       <Layout>
-        <AdImgSlider />
-        <Categories />
-        <Headings heading={"New Arrivals"} data={"newarrivals"} />
-        <Headings heading={"Top Sellers"} data={"topsellers"} />
+        {isLoading ? (
+          <ClockLoader
+            loading={true}
+            color="#ea5a6dff"
+            cssOverride={override}
+            speedMultiplier={1}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+            size={150}
+          />
+        ) : (
+          <>
+            <AdImgSlider />
+            <Categories />
+            <Headings heading={"New Arrivals"} data={"newarrivals"} />
+            <Headings heading={"Top Sellers"} data={"topsellers"} />
+          </>
+        )}
+
         {/* <Searchbar />
         <Slidebar /> */}
       </Layout>
